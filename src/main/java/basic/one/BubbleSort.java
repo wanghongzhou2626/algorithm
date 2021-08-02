cda85498483c2fdbca2cb7a7cf28d8040890bd77
âwhz <1012308609@qq.com>
2021-08-02 16:54:10 +0800

    bubble sortâ

---
+++ src/main/java/basic/one/BubbleSort.java
@@ -0,0 +1,11 @@
+package basic.one;
+
+/**
+ *
+ * 冒泡排序 基本思想 0 位置与 1 位置比较 谁大移动，1位置与2位置谁大谁移动  一圈以后确定元素中最大值在末尾
+ * 时间复杂度  O(n^2)  空间复杂度 O(n^2) 有限几个变量
+ *
+ * @author whz
+ */
+public class BubbleSort {
+}

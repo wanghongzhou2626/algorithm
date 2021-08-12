eaf0b5e11c38eb866ff6bd2b29960cb1b6e77198
âwhz <1012308609@qq.com>
2021-08-12 15:40:07 +0800

    linkedlist palindrome find mid push stack code commit

--- src/main/java/basic/six/LinkedListPalindrome.java
+++ src/main/java/basic/six/LinkedListPalindrome.java
@@ -25,20 +25,52 @@ public class LinkedListPalindrome {
      * @return
      */
     public static boolean linkedListPalindrome(Node head){
-        if (head == null){
-            return false;
-        }
         Stack<Node> stack = new Stack<Node>();
-
-        while (head != null){
-            stack.push(head);
-            head = head.next;
+        Node cur = head;
+        while (cur != null){
+            stack.push(cur);
+            cur = cur.next;
         }
 
         while (head != null){
             if(stack.pop().value != head.value){
                 return false;
             }
+            head = head.next;
+        }
+        return true;
+    }
+
+
+    /**
+     * 快慢指针 找中点 中点后位置压栈 比对
+     */
+    public static boolean isPalindromeMid(Node head){
+        //如果他只有一个元素 不管他是啥 都算是回文
+        if(head == null || head.next == null){
+            return true;
+        }
+
+        //设置快慢指针
+        Node slow = head.next;
+        Node fast = head.next;
+        while (fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        //循环后的slow就是中点位置了 如果节点数为奇数那么返回的就是中点位置 如果节点数为偶数返回的就是下中点位置
+        //现在将slow后续的节点压入栈中
+        Stack<Node> stack = new Stack<Node>();
+        while (slow != null){
+            stack.push(slow);
+            slow = slow.next;
+        }
+        //拿出栈中数据 与 链表头节点开始作比较
+        while (!stack.isEmpty()){
+            if(head.value != stack.pop().value){
+                return false;
+            }
+            head = head.next;
         }
         return true;
     }

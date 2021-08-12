38fdf482ebac79f9f5869dd76fa37a2c0bf61ed9
âwhz <1012308609@qq.com>
2021-08-12 18:38:01 +0800

    fid small equals big part in linkedList

--- src/main/java/basic/six/LinkedListPartition.java
+++ src/main/java/basic/six/LinkedListPartition.java
@@ -97,4 +97,86 @@ public class LinkedListPartition {
         nodes[i].value = nodes[j].value;
         nodes[j].value = temp;
     }
+
+
+    public static Node linkedListPartition(Node head, int value){
+        //创建六个指针 分别代表 小于区域头节点 尾节点
+        // 大于区域 头节点 尾节点
+        // 等于区域 头节点 尾节点
+        Node sH = null;
+        Node sT = null;
+
+        Node eH = null;
+        Node eT = null;
+
+        Node bH = null;
+        Node bT = null;
+
+        Node next = null;
+        while (head != null) {
+            //记录一下 下个节点的环境
+            next = head.next;
+            //断开当前节点与后续节点的联系 你已经用next变量存储了环境 为啥断开 你现在是单独拿出某个节点 往三个区域去发货 并建立新的连接
+            head.next = null;
+            //如果当前节点的值小于划分值那么规划到小于区域中
+            if(head.value < value){
+                //如果小于区域的头节点为空说明还没有元素呢  头节点和尾节点都是当前节点
+                if(sH == null){
+                    sH = head;
+                    sT = head;
+                }else {
+                    //说明此时的小于区域已经存在节点了 怎么把他连进去 并且当前节点为新的尾节点
+                    //当前小于区域的尾节点的next指针 应该指向当前节点
+                    sT.next = head;
+                    //当前节点 应该变成尾节点
+                    sT = head;
+                }
+                //如果当前划分值 与 当前节点值相等 我应该怎么办 还是加入到等于区域 并且也是两种情况
+            } else if (head.value == value) {
+                //如果我小于区域此时还没有值呢 头尾都指向当前节点即可
+                if(eT == null) {
+                    eH = head;
+                    eT = head;
+                }else {
+                    //说明有节点了 那么就得插到等于区的尾部节点  并且当前节点为尾部
+                    eT.next = head;
+                    eT = head;
+                }
+            } else {
+                //此时的当前节点应该去大于区域
+                // 如果大于区域为空 直接头尾都指向当前节点
+                if(bH == null) {
+                    bH = head;
+                    bT = head;
+                }else {
+                    bT.next = head;
+                    bT = head;
+                }
+            }
+            head = next;
+
+        }
+
+        //此时的链表分成了三个区域 小于 等于 大于 现在需要把 小于区域的尾 连到等于区域的头 等于区域的尾连到大于区域的头
+        //但是要考虑各种情况
+
+        //如果小于区域尾部不为空 说明小于区域有节点 用小于区域的尾巴连等于区域的头
+        if(sT != null) {
+            sT.next = eH;
+            //谁去连大约区域的头 谁就变成eT
+            eT = eT == null ? sT : eT;
+
+        }
+        //下一步 需要用eT去连接大于区域的头 此时的et可能就是et 但是这个et也是有可能为null的  也有可能是小于区域的头
+        //说明我有自己的et
+        if(eT != null){
+            //等于区域的尾连大于区域的头
+            eT.next = bH;
+        }
+
+        //如果小于区域头不为空 返回小于区域的头
+        // 如果小于区域头为空 看等于区域头是否为空 不为空返回等于区域头 否则返回 大约区域头
+        return sH != null ? sH : (eH != null ? eH :bH);
+
+    }
 }

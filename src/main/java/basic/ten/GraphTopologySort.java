8454a8065988f2b17ac856b2abd060bff607bfd6
âwhz <1012308609@qq.com>
2021-08-24 15:32:25 +0800

    graph topologySort code commit

--- src/main/java/basic/ten/GraphTopologySort.java
+++ src/main/java/basic/ten/GraphTopologySort.java
@@ -1,17 +1,42 @@
 package basic.ten;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.*;
 
 /**
  * 图的拓扑排序
- * 并查集
+ * <p>
+ * 在一张有向无环图中 寻找入度为零的节点 并把他的next节点-1 在寻找下一个入度为零的节点  得到的result 就是图的拓扑排序
  *
  * @author whz
  */
 public class GraphTopologySort {
 
-    public static void main(String[] args) {
+
+    public static List<Node> sort(Graph graph) {
+        HashMap<Node, Integer> nodeIn = new HashMap<Node, Integer>();
+        Queue<Node> zeroInQueue = new LinkedList<Node>();
+
+        for (Node node : graph.nodes.values()) {
+            nodeIn.put(node, node.in);
+            //构造数据时一定会有一个入度为0的点  把他放入一个队列中
+            if (node.in == 0) {
+                zeroInQueue.add(node);
+            }
+        }
+
+        ArrayList<Node> result = new ArrayList<Node>();
+        while (!zeroInQueue.isEmpty()) {
+            Node cur = zeroInQueue.poll();
+            result.add(cur);
+            for (Node next : cur.nexts) {
+                nodeIn.put(next, nodeIn.get(next) - 1);
+                if (nodeIn.get(next) == 0) {
+                    zeroInQueue.add(next);
+                }
+            }
+        }
+        return result;
 
     }
+
 }
---
+++ src/main/java/basic/ten/Kruskal.java
@@ -0,0 +1,11 @@
+package basic.ten;
+
+/**
+ * @author whz
+ *
+ * 图的最小生成树算法
+ *
+ *
+ */
+public class Kruskal {
+}

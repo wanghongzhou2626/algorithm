093dd68e849d4c022b264163678c7f8d75c66fab
âwhz <1012308609@qq.com>
2021-08-24 15:01:42 +0800

    graph dfs code commit

--- src/main/java/basic/ten/GraphDFS.java
+++ src/main/java/basic/ten/GraphDFS.java
@@ -1,5 +1,8 @@
 package basic.ten;
 
+import java.util.HashSet;
+import java.util.Stack;
+
 /**
  * @author whz
  *
@@ -8,4 +11,33 @@ package basic.ten;
  * set作用记录走过的路 防止重复走
  */
 public class GraphDFS {
+
+    /**
+     * 给定一个图的节点  深度优先遍历图算法
+     * @param node
+     */
+    public static void dfs(Node node){
+        if(node == null){
+            return;
+        }
+        Stack<Node> nodeStack = new Stack<Node>();
+        HashSet<Node> set = new HashSet<Node>();
+        //第一个节点两个容器都进入
+        nodeStack.add(node);
+        set.add(node);
+        //打印
+        System.out.println(node.value);
+        while (!nodeStack.isEmpty()){
+            Node cur = nodeStack.pop();
+            for (Node next: cur.nexts) {
+                if(!set.contains(next)){
+                    nodeStack.push(cur);
+                    nodeStack.push(next);
+                    set.add(next);
+                    System.out.println(next.value);
+                    break;
+                }
+            }
+        }
+    }
 }

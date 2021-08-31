2046a2946d490221aa7f7e950754a05c018fea1d
âwhz <1012308609@qq.com>
2021-08-31 22:06:33 +0800

    button code commit

--- src/main/java/basic/one/Review_InsertSort.java
+++ src/main/java/basic/one/Review_InsertSort.java
@@ -13,7 +13,7 @@ public class Review_InsertSort {
             return;
         }
         /**
-         * 1->n
+         * 0-i有序
          */
         for (int i = 1; i < arr.length ; i++) {
             for (int j = i - 1; j >=0 && arr[j] > arr[j+1] ; j--) {

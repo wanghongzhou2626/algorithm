ad424bdbad9e6ada46d902f1933215a318c8ddd2
âwhz <1012308609@qq.com>
2021-08-31 21:59:54 +0800

    insert sort code commit

---
+++ src/main/java/basic/one/Review_InsertSort.java
@@ -0,0 +1,30 @@
+package basic.one;
+
+
+/**
+ * review insert sort
+ * 思路 每次向前看 直到不能在交换了 打扑克牌插入
+ */
+public class Review_InsertSort {
+
+
+    public static void insertSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        /**
+         * 1->n
+         */
+        for (int i = 1; i < arr.length ; i++) {
+            for (int j = i - 1; j >=0 && arr[j] > arr[j+1] ; j--) {
+                swap(arr,j,j+1);
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
